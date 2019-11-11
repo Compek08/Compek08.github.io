101d23198c9ef6460401a9424b907286a3ad788b
Compek <compek@dr.com>
2019-11-12 05:41:22 +0700

    compek

--- public_html/assets/js/js.js
+++ public_html/assets/js/js.js
@@ -64,8 +64,81 @@ $(document).ready(function () {
             readURL(this);
         });
     };
-    
+
     $('.image').compekFile();
+
+    $.fn.validate = function () {
+        this.submit(function (ev) {
+            ev.stopPropagation();
+            retun = true;
+            $("form#form :input").each(function () {
+                name = $(this).attr('name');
+                if (name === 'alamat') {
+                    value = $(this).html();
+                } else {
+                    value = $(this).val();
+                }
+                console.log(name, value);
+                regex = '';
+                kosong = $.isEmptyObject(value) && name !== 'profilePic';
+                if (name === 'nama') {
+                    regex = /^[a-zA-Z]+( *[a-zA-Z']+)*$/;
+                    pesan = "Hanya Boleh Memasukkan Format Nama";
+                } else
+
+                if (name === 'nis') {
+                    regex = /^[0-9]{8}$/;
+                    pesan = "Hanya Boleh Memasukkan Format Angka 8 Digit";
+                } else
+
+                if (name === 'alamat') {
+                    regex = /^([j][l][.][ ])([a-z0-9 .])+((([ ][n][o][.][ ])|([\/]))[0-9]+[a-z]*)$/i;
+                    pesan = "Masukan Alamat yang Benar";
+                } else
+
+                if (name === 'email') {
+                    regex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+                    pesan = "Masukan Format Alamat E-mail yang Benar";
+                } else
+
+                if (name === 'gender') {
+                    regex = /((Laki-laki)|(Perempuan))$/;
+                    pesan = "Masukan Jenis Kelamin";
+                } else
+
+                if (name === 'password') {
+                    regex = /([\s\S]{8,})$/;
+                    pesan = "Password Minimal 8 Digit";
+                } else
+
+                if (name === 'profilePic') {
+                    regex = /([\s\S])+((.jpg)|(.jpeg)|(.tiff)|(.gif)|(.png)|(.bmp))$/i;
+                    pesan = "File Harus Gambar";
+                } else
+
+                if (kosong) {
+                    pesan = "Tidak Boleh Kosong";
+                }
+
+                if (!value.match(regex) || kosong) {
+                    console.log(pesan);
+                    $(this).addClass('error');
+                    retun = false;
+                    return false;
+                } else {
+                    $(this).removeClass('error');
+                }
+            });
+            console.log(retun);
+            if (retun) {
+                return true;
+            } else {
+            return false;
+            }
+        });
+    };
+
+    $('#form').validate();
 
     document.title = readStorage() + " MELSA";
 });
